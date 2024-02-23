9e8cea076e8af657972618e2ca5c345bbeb0e345
Will Robinson <willrobinsononon@yahoo.com>
2024-02-23 15:01:35 +0000

    added models

--- JobAPI/models.py
+++ JobAPI/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Application(models.Model):
 
+    STATUS_CHOICES = {
+    "APL": "Applied",
+    "INT": "Interview Booked",
+    "SCS": "SUCCESSFUL",
+    "UNS": "UNSUCCESSFUL"
+    }
+    
+    date_applied = models.DateField()
+    job_title = models.CharField(max_length = 32)
+    employer = models.CharField(max_length = 32)
+    notes = models.TextField()
+    followed_up = models.BooleanField(default = False)
+    status = models.CharField(max_length = 2, choices = STATUS_CHOICES, default="APL")
+
+class Interview(models.Model):
+    scheduled_time = models.DateTimeField()
+    application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name = "interviews")
