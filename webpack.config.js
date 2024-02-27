af1f77d43e89ddac04a0b09877927d7abf0f614f
Will Robinson <willrobinsononon@yahoo.com>
2024-02-27 16:04:18 +0000

    change to use a bundled react app with webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/index.js',  // path to our input file
+  output: {
+    filename: 'app-bundle.js',  // output bundle file name
+    path: path.resolve(__dirname, './static'),  // path to our Django static directory
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+      },
+    ]
+  }
+};
+
