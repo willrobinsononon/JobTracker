4ad172f8727c76295bbf8fab0bc565b19423025d
Will Robinson <willrobinsononon@yahoo.com>
2024-03-13 09:17:01 +0000

    allow blank notes

--- JobAPI/models.py
+++ JobAPI/models.py
@@ -12,7 +12,7 @@ class Application(models.Model):
     date_applied = models.DateField()
     job_title = models.CharField(max_length = 32)
     employer = models.CharField(max_length = 32)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
     followed_up = models.BooleanField(default = False)
     status = models.CharField(max_length = 3, choices = STATUS_CHOICES, default="APL")
     owner = models.ForeignKey('auth.User', related_name='applications', on_delete=models.CASCADE)
@@ -20,6 +20,6 @@ class Application(models.Model):
 class Interview(models.Model):
     scheduled_time = models.DateTimeField()
     location = models.CharField(max_length = 128)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
     application = models.ForeignKey(Application, on_delete=models.CASCADE, related_name = "interviews")
 
